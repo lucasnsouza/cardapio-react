577eaae2e84011c9da18f7ea8fc84f919d3cff45
lucasnsouza <lucasnstj@gmail.com>
2022-06-14 15:09:48 -0300

    feat: buscador e filtros totalmente funcionais

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,43 @@
 import styles from "./Itens.module.scss";
 import cardapio from "./itens.json";
 import Item from "./Item";
+import { useEffect, useState } from "react";
 
-export default function Itens() {
+interface Props {
+  busca: string,
+  filtro: number | null,
+  ordenador: string
+}
+
+export default function Itens({ busca, filtro, ordenador}: Props) {
+  //lista de pratos a ser exibida
+  const [lista, setLista] = useState(cardapio);
+
+  function testaBusca(title: string) {
+    //definindo regex case insentive
+    //não faz diferença maiúsculas e minúsculas 
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null) {
+      return filtro === id;
+    }
+    //se o filtro for vazio, ou null
+    return true;
+  }
+
+  //sempre que busca e filtro mudarem, useEffect atualiza
+  useEffect(() => {
+    //lsita de pratos a ser exibida de acordo com busca e filtros
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(novaLista);
+  }, [busca, filtro]);
+
   return (
     <div className={styles.itens}>
-      {cardapio.map((item) => (
+      {lista.map((item) => (
         <Item 
           key={item.id}
           {...item}
